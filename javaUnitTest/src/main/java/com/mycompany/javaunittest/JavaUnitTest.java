027a45813bfe39c3f7f8e69295aa50cadc438db0
benjamin perrin <perrin.benjamin77@gmail.com>
2022-07-21 15:00:39 +0200

    base test

--- javaUnitTest/src/main/java/com/mycompany/javaunittest/JavaUnitTest.java
+++ javaUnitTest/src/main/java/com/mycompany/javaunittest/JavaUnitTest.java
@@ -14,4 +14,6 @@ public class JavaUnitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    
 }
---
+++ javaUnitTest/src/test/java/com/mycompany/javaunittest/JavaUnitTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javaunittest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author ben
+ */
+public class JavaUnitTest {
+    
+    @Test
+    public void firstTestMethod(){
+        assertTrue(true); 
+    }
+}
